4584916e353cb326e62bacfb3bfac1f20aa3ef25
Kumudu Gunasekara <kumudug1@gmail.com>
2019-04-27 14:13:18 -0700

    Synchronously upload file.

--- server.js
+++ server.js
@@ -7,23 +7,26 @@ const server = http.createServer((req, res) => {
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
     res.write('</html>');
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
     const body = [];
-    req.on('data', (chunk) => {
+    req.on('data', chunk => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-      res.statusCode = 302;
-      res.setHeader('Location', '/');
-      return res.end();
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
   }
   res.setHeader('Content-Type', 'text/html');
